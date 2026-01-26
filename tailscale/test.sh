0274ac7a6f9896049ddb525fdd4809522af2bf64
coobra <velpulasudheer0@gmail.com>
2026-01-26 21:11:45 +0530

    added testcase and modified

--- tailscale/test.sh
+++ tailscale/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-output=$(./helloworld.sh)
+output=$(./helloworld.sh apple)
 # echo "$output"
-if [ "$output" = "Hello, World!" ]; then
+if [ "$output" = "Hello, World! apple" ]; then
   echo "Test Passed ✅"
 else
   echo "Test Failed ❌"
-  echo "Expected: Hello, World!"
+  echo "Expected: Hello, World! apple"
   echo "Got: $output"
 fi
